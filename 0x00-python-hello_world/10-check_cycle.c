429509a2addfdb61f06633b9d2fef212bdecc719
zee-ham-su <hsufiian@yahoo.com>
2023-06-05 06:06:23 -0700

    check cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,7 +11,7 @@ int check_cycle(listint_t *head)
 listint_t *current = head;
 listint_t *runner = head;
 
-if (!head || !head->next)
+ if (!head)
 return (0);
 
 while (current && runner && runner->next)
