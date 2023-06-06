78c2828e78365c8d7b8666437837b436d91c8c1f
zee-ham-su <hsufiian@yahoo.com>
2023-06-06 08:14:11 -0700

    combo

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if x != y:
+            print("{:02d} ".format(x * 10 + y), end="")
+            if x != 8 or y != 9:
+                print(", ", end="")
+
+print()
