4720337344599ef0ad1e7556e9c6cee0b608b4e9
zee-ham-su <hsufiian@yahoo.com>
2023-06-06 07:34:46 -0700

    q and e not called to the party

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -3,5 +3,5 @@ for i in range(97, 123):
     crrt_ltr = 97
 while crrt_ltr <= 122:
     if crrt_ltr != 113 and crrt_ltr != 101:
-        print(chr(crrt_ltr), end="")
+        print("{0}".format(chr(crrt_ltr)), end="")
     crrt_ltr += 1
