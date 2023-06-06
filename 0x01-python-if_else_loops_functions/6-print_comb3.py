e192b5c23f84e697008ad81022faa2ebedbe1734
zee-ham-su <hsufiian@yahoo.com>
2023-06-06 08:19:57 -0700

    combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for x in range(0, 10):
     for y in range(x + 1, 10):
         if x != y:
-            print("{:02d} ".format(x * 10 + y), end="")
+            print("{:02d}".format(x * 10 + y), end="")
             if x != 8 or y != 9:
-                print(", ", end="")
+                print(",", end="")
 
 print()
