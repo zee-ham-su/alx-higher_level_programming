a7a98f8a6d1f09a44361f7789fabee90cfa05042
zee-ham-su <hsufiian@yahoo.com>
2023-06-08 05:12:56 -0700

    arithmetic calculations update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
 
-total_sum = add(a, b)
-total_sub = sub(a, b)
-total_mul = sub(a, b)
-total_div = div(a, b)
+    total_sum = add(a, b)
+    total_sub = sub(a, b)
+    total_mul = sub(a, b)
+    total_div = div(a, b)
 
-print('{:d} + {:d} = {:d}'.format(a, b, total_sum))
-print('{:d} - {:d} = {:d}'.format(a, b, total_sub))
-print("{} * {} = {}".format(a, b, total_mul))
-print("{} / {} = {}".format(a, b, total_div))
+    print('{:d} + {:d} = {:d}'.format(a, b, total_sum))
+    print('{:d} - {:d} = {:d}'.format(a, b, total_sub))
+    print("{} * {} = {}".format(a, b, total_mul))
+    print("{} / {} = {}".format(a, b, total_div))
