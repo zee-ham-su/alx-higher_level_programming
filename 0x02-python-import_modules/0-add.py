4a561d92628e35d17b7a149c29f36fbd787c0ac9
zee-ham-su <hsufiian@yahoo.com>
2023-06-08 05:09:50 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-total = add(a, b)
-print("{} + {} = {}".format(a, b, total))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    total = add(a, b)
+    print("{} + {} = {}".format(a, b, total))
