c711906531f102533caebc9f0917d78cdf1063ef
zee-ham-su <hsufiian@yahoo.com>
2023-06-08 08:09:30 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
-
     for name in dir(hidden_4):
         if not name.startswith("_"):
             value = getattr(hidden_4, name)
