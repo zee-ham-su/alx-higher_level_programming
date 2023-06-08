e4afccbeb51093ef723c698a68047e128fb630e4
zee-ham-su <hsufiian@yahoo.com>
2023-06-08 06:04:34 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     import hidden_4
 
     for name in dir(hidden_4):
-        if not name.startswith("__"):
+        if not name.startswith("_"):
             value = getattr(hidden_4, name)
             if not callable(value):
                 print(name)
