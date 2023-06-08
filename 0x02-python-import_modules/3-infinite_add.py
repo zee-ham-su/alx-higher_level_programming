1d9d0940f15fad767930d3a9c0d0f736c324d493
zee-ham-su <hsufiian@yahoo.com>
2023-06-08 05:46:12 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import sys
 
+"""Adds all arguments and prints the sum."""
 
-def add_arguments():
-    """Adds all arguments and prints the result."""
-    args = sys.argv[1:]
-    int_args = [int(arg) for arg in args]
-    sum_args = sum(int_args)
-    print(sum_args)
+args = sys.argv[1:]
+int_args = [int(arg) for arg in args]
+sum_args = sum(int_args)
+print(sum_args)
 
 
 if __name__ == "__main__":
-    add_arguments()
+    pass
