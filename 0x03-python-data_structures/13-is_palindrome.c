4fd72ec6e88952a31eaed7eaab37163d2cd322ff
zee-ham-su <hsufiian@yahoo.com>
2023-06-13 05:06:06 -0700

    palindrome update updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,114 +1,71 @@
-#include <stddef.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * copy_listint - creates a copy of a linked list
+ * reverse_linked_list - to reverse the order of nodes
+ * in a linked list.
  * @head: pointer to the first node in the list
  *
- * Return: pointer to the new list
+ * Returns: returns a pointer to the first node of
+ * the reversed list.
  */
-
-listint_t *copy_listint(listint_t *head)
+void reverse_linked_list(listint_t **head)
 {
-listint_t *new_list = NULL;
-listint_t *current = head;
-listint_t *tail = NULL;
-
-while (current)
-{
-listint_t *new_node = malloc(sizeof(listint_t));
-if (!new_node)
-{
-free_listint(new_list);
-return (NULL);
-}
-
-new_node->n = current->n;
-new_node->next = NULL;
-
-if (!new_list)
-{
-new_list = new_node;
-tail = new_list;
-}
-else
-{
-tail->next = new_node;
-tail = tail->next;
-}
-
-current = current->next;
-}
-
-return (new_list);
-}
-
-/**
- * reverse_listint - Reverses a linked list.
- * @head: A double pointer to the head of the linked list.
- *
- * Return: A pointer to the new head of the reversed list.
- */
-
-listint_t *reverse_listint(listint_t **head)
-{
-listint_t *prev = NULL;
 listint_t *current = *head;
-listint_t *next = NULL;
-
+listint_t *next_node = NULL;
+listint_t *last = NULL;
 while (current != NULL)
 {
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
+next_node = current->next;
+current->next = last;
+last = current;
+current = next_node;
 }
 
-*head = prev;
-return (*head);
-}
-
-/**
- * compare_lists - Compares two linked lists for equality.
- * @list1: The first linked list.
- * @list2: The second linked list.
- *
- * Return: 1 if the lists are equal, 0 otherwise.
- */
-
-int compare_lists(listint_t *list1, listint_t *list2)
-{
-while (list1 != NULL && list2 != NULL)
-{
-if (list1->n != list2->n)
-return (0);
-
-list1 = list1->next;
-list2 = list2->next;
-}
-
-if (list1 == NULL && list2 == NULL)
-return (1);
-else
-return (0);
+*head = last;
 }
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
  * @head: double pointer to the linked list
  *
- * Return: 1 if it is a palindrome, 0 if not
+ * Return: 0 if it is not a palindrome and 1 if it is
  */
-
 int is_palindrome(listint_t **head)
 {
-listint_t *list_copy = copy_listint(*head);
-reverse_listint(&list_copy);
-
-int is_palindrome = compare_lists(*head, list_copy);
-
-free_listint(list_copy);
-
-return (is_palindrome);
+listint_t *slow_ptr = *head, *fast_ptr = *head,
+*temp_ptr = *head, *second_half = NULL;
+if (!*head || !(*head)->next)
+return (1);
+while (1)
+{
+fast_ptr = fast_ptr->next->next;
+if (!fast_ptr)
+{
+second_half = slow_ptr->next;
+break;
 }
+if (!fast_ptr->next)
+{
+second_half = slow_ptr->next->next;
+break;
+}
+slow_ptr = slow_ptr->next;
+}
+reverse_linked_list(&second_half);
+while (second_half != NULL && temp_ptr != NULL)
+{
+if (temp_ptr->n == second_half->n)
+{
+second_half = second_half->next;
+temp_ptr = temp_ptr->next;
+}
+else
+{
+return (0);
+}
+}
+if (second_half == NULL)
+return (1);
+return (0);
+}
+
