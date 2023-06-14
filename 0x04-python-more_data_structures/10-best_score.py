fc8eac1d553f5b510664ae8b46b4c250caa2d795
zee-ham-su <hsufiian@yahoo.com>
2023-06-14 09:18:36 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,9 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    best_score = max(a_dictionary.values())
-    best_key = next((key for key, value in a_dictionary.items()
-                     if value == best_score), None)
-    return best_key
+    scores = sorted(a_dictionary.values(), reverse=True)
+    if scores:
+        return next(key for key,
+                    value in a_dictionary.items() if value == scores[0])
+    else:
+        return None
