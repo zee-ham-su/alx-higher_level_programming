a9f64a3abba70e73be2acad4be748f93685e9048
zee-ham-su <hsufiian@yahoo.com>
2023-06-26 20:49:30 +0000

    python

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -10,21 +10,20 @@ void print_python_list(PyObject *p)
 {
 if (!PyList_Check(p))
 {
-printf("[ERROR] Invalid PyListObject\n");
+fprintf(stderr, "Invalid PyListObject\n");
 return;
 }
 
 Py_ssize_t size = PyList_Size(p);
-Py_ssize_t allocated = ((PyListObject *)p)->allocated;
-
 printf("[*] Python list info\n");
 printf("[*] Size of the Python List = %ld\n", size);
-printf("[*] Allocated = %ld\n", allocated);
+
+printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 for (Py_ssize_t i = 0; i < size; i++)
 {
 PyObject *item = PyList_GetItem(p, i);
-const char *item_type = Py_TYPE(item)->tp_name;
+const char *item_type = item->ob_type->tp_name;
 printf("Element %ld: %s\n", i, item_type);
 }
 }
@@ -38,26 +37,26 @@ void print_python_bytes(PyObject *p)
 {
 if (!PyBytes_Check(p))
 {
-printf("[ERROR] Invalid Bytes Object\n");
+fprintf(stderr, "Invalid PyBytesObject\n");
 return;
 }
 
-Py_ssize_t size = PyBytes_GET_SIZE(p);
-const char *bytes_data = PyBytes_AsString(p);
+Py_ssize_t size = PyBytes_Size(p);
+const char *str = PyBytes_AsString(p);
 
 printf("[.] bytes object info\n");
 printf("  size: %ld\n", size);
-printf("  trying string: %s\n", bytes_data);
-printf("  first 6 bytes: ");
+printf("  trying string: %s\n", str);
 
-Py_ssize_t limit = size > 6 ? 6 : size;
-for (Py_ssize_t i = 0; i < limit; i++)
+if (size > 10)
+size = 10;
+printf("  first %ld bytes: ", size);
+for (Py_ssize_t i = 0; i < size; i++)
 {
-printf("%02hhx", bytes_data[i]);
-if (i < limit - 1)
+printf("%02hhx", str[i]);
+if (i < size - 1)
 printf(" ");
 }
-
 printf("\n");
 }
 
@@ -70,11 +69,11 @@ void print_python_float(PyObject *p)
 {
 if (!PyFloat_Check(p))
 {
-printf("[ERROR] Invalid Float Object\n");
+fprintf(stderr, "Invalid PyFloatObject\n");
 return;
 }
 
-double value = PyFloat_AS_DOUBLE(p);
+double value = PyFloat_AsDouble(p);
 
 printf("[.] float object info\n");
 printf("  value: %f\n", value);
