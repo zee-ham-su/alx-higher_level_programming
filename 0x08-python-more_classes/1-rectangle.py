3815419f0af1f9b4c952cc985868109a71cf5672
zee-ham-su <hsufiian@yahoo.com>
2023-07-03 05:38:59 -0700

    reactangle definition

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,12 +8,12 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a rectangle object with given width and height."""
-        self._width = width
-        self._height = height
+        self.__width = width
+        self.__height = height
 
     def get_width(self):
         """Retrieve the width of the rectangle."""
-        return self._width
+        return self.__width
 
     def set_width(self, value):
         """Set the width of the rectangle."""
@@ -21,11 +21,11 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     def get_height(self):
         """Retrieve the height of the rectangle."""
-        return self._height
+        return self.__height
 
     def set_height(self, value):
         """Set the height of the rectangle."""
@@ -33,7 +33,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
 
     width = property(get_width, set_width)
     height = property(get_height, set_height)
