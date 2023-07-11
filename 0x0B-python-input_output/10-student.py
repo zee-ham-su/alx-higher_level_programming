13b58df0ab84847ce7c8a3a9bbc1d4c34429a080
zee-ham-su <hsufiian@yahoo.com>
2023-07-11 08:44:31 -0700

    update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pthon3
-"""class Student that defines a student by: (based on 9-student.py)
+"""class Student that defines a student
 """
 
 
@@ -10,18 +10,14 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """init a new Student
         """
-
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
         """ Method that returns directory description """
-        if attrs is None or not isinstance(attrs, list):
-            return self.__dict__
-        else:
-            selected_attrs = {}
-            for attr in attrs:
-                if attr in self.__dict__:
-                    selected_attrs[attr] = self.__dict__[attr]
-            return selected_attrs
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs
+                    if hasattr(self, attr)}
+        return self.__dict__
