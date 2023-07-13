bc360bf7aa2524e05d26d89829affac33cedfc6d
zee-ham-su <hsufiian@yahoo.com>
2023-07-13 10:27:39 -0700

    model update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -83,8 +83,10 @@ class Rectangle(Base):
         """prints in stdout the Rectangle instance with the
         character #
         """
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
-            print("#" * self.width)
+            print(" " * self.x + "#" * self.width)
 
     def __str__(self):
         """String representation of the Rectangle instance.
