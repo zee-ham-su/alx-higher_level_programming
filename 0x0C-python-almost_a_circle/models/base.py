082966afdfbed31042f87e0e2f43f897d4030313
zee-ham-su <hsufiian@yahoo.com>
2023-07-14 10:04:33 -0700

    model update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -10,7 +10,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """inita new base"""
+        """init new base"""
         if id is not None:
             self.id = id
         else:
@@ -37,3 +37,11 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 file.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation
+        json_string"""
+        if json_string is None or json_string == "[]":
+            return[]
+        return json.loads(json_string)
