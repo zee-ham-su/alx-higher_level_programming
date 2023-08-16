a782a9581fbd1fa713ddb606e325d1e1602bfce4
Sufian Hamza <hsufiian@yahoo.com>
2023-08-16 13:58:13 +0000

    read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,7 +6,7 @@
 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'%';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
 
 
