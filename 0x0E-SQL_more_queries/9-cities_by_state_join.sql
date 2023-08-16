7f233ce318c2fd6cef8ad629051493f83292b032
Sufian Hamza <hsufiian@yahoo.com>
2023-08-16 16:05:57 +0000

    cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -7,4 +7,5 @@ USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id;
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
