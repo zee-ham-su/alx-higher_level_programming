1a1971749eb1d80998c8082d06d8ed829d416807
Sufian Hamza <hsufiian@yahoo.com>
2023-09-11 13:12:43 +0000

    argc

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const argCount = process.argv.length;
 
 if (argCount === 2) {
-    console.log('No argument');
-} else if (argCount === 3 ) {
-    console.log('Argument found');
+  console.log('No argument');
+} else if (argCount === 3) {
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
