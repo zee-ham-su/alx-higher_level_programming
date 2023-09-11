2d60a6e98665a716f2cf27ba83d424ceddf7883b
zee-ham-su <hsufiian@yahoo.com>
2023-09-11 08:32:56 -0700

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const factorial = (n) => {
+function factorial (n) {
   if (isNaN(n)) return 1;
   if (n <= 1) return 1;
   return n * factorial(n - 1);
-};
+}
 
 const arg1 = process.argv[2];
 const num = parseInt(arg1);
