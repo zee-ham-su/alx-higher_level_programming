789aa18047d29d5e94d7fcffd7d82597fcb7b409
zee-ham-su <hsufiian@yahoo.com>
2023-09-14 10:30:52 -0700

    task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,35 +4,25 @@ database `hbtn_0e_0_usa`.
 """
 
 import MySQLdb
-import sys
-
+from sys import argv
 
 if __name__ == '__main__':
-    if len(argv) != 4:
-        print("Usage: {} <username> <password> <database_name>".format(argv[0]))
-        exit(1)
-
     try:
-
         db_connect = MySQLdb.connect(
-            host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
-
+            host="localhost", user=argv[1], port=3306,
+            passwd=argv[2], db=argv[3])
 
         db_cursor = db_connect.cursor()
 
         db_cursor.execute("SELECT * FROM states")
 
-        rows_selected = db_cursor.fetchall()
+        rows = db_cursor.fetchall()
 
-        for row in rows_selected:
+        for row in rows:
             print(row)
+
+        db_cursor.close()
+        db_connect.close()
 
     except MySQLdb.Error as e:
         print("MySQL Error:", e)
-
-    finally:
-        if 'db_cursor' in locals() and db_cursor is not None:
-            db_cursor.close()
-
-        if 'db_connect' in locals() and db_connect is not None:
-            db_connect.close()
