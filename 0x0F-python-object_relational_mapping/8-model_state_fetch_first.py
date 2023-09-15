d2f3c725be8e3fc1922ef8ee117d83bd1bacef5d
zee-ham-su <hsufiian@yahoo.com>
2023-09-15 08:37:16 -0700

    fetch one

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,40 +1,19 @@
 #!/usr/bin/python3
-"""Script to print the first State object from the
-database hbtn_0e_6_usa.
-"""
-
-import MySQLdb
+"""Prints the first State object from the database hbtn_0e_6_usa."""
 import sys
 from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(
-            sys.argv[0]))
-        sys.exit(1)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).first()
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    try:
-        db_connect = MySQLdb.connect(
-            host="localhost", user=username, passwd=password, db=database
-        )
-
-        db_cursor = db_connect.cursor()
-
-        db_cursor.execute("SELECT * FROM states ORDER BY id ASC LIMIT 1")
-        first_state = db_cursor.fetchone()
-
-        if first_state is None:
-            print("Nothing")
-        else:
-            state = State(id=first_state[0], name=first_state[1])
-            print("{}: {}".format(state.id, state.name))
-
-        db_cursor.close()
-        db_connect.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+    if instance is None:
+        print("Nothing")
+    else:
+        print(instance.id, instance.name, sep=": ")
