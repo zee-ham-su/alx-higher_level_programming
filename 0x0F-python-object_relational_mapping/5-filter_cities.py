6ec2e7771e1d37d97bde19775a2cb704aa57c228
zee-ham-su <hsufiian@yahoo.com>
2023-09-15 02:23:10 -0700

    task 5 corr

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,11 +15,9 @@ if __name__ == '__main__':
 
         db_cursor = db_connect.cursor()
 
-        db_cursor.execute(
-            "SELECT cities.name FROM cities "
+        db_cursor.execute("""SELECT cities.name FROM cities "
             "INNER JOIN states ON states.id = cities.state_id "
-            "WHERE states.name = %s", (sys.argv[4],)
-        )
+            "WHERE states.name = %s""", (sys.argv[4],))
 
         states = db_cursor.fetchall()
 
