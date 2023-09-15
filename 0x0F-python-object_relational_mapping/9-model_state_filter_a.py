1a6389bfe9b8db87908385a599644c08fd432cef
zee-ham-su <hsufiian@yahoo.com>
2023-09-15 09:36:25 -0700

    state filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,26 +8,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                               format(username, password, database),
-                               pool_pre_ping=True)
-
-
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
     states = (session.query(State)
-            .filter(State.name.like('%a%'))          
+              .filter(State.name.like('%a%')))
 
-    if states is not None:
+    if states.count() > 0:
         for state in states:
             print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
 
     session.close()
-
-    except Exception as e:
-        print("Error:", e)
