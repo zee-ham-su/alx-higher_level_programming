7d9e48b788d251b29fc603f8d7bc43b1c472eb8c
zee-ham-su <hsufiian@yahoo.com>
2023-09-15 07:41:27 -0700

    filter correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,23 +7,19 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    try:
-        db_connect = MySQLdb.connect(
-            host="localhost", user=argv[1], port=3306,
-            passwd=argv[2], db=argv[3])
+    db_connect = MySQLdb.connect(
+        host="localhost", user=argv[1], port=3306,
+        passwd=argv[2], db=argv[3])
 
-        db_cursor = db_connect.cursor()
+    db_cursor = db_connect.cursor()
 
-        db_cursor.execute("""SELECT * FROM states WHERE name
-                          LIKE 'N%' ORDER BY states.id""")
+    db_cursor.execute("""SELECT * FROM states WHERE name
+                      LIKE 'N%' ORDER BY states.id""")
 
-        rows = db_cursor.fetchall()
+    rows = db_cursor.fetchall()
 
-        for row in rows:
-            print(row)
+    for row in rows:
+        print(row)
 
-        db_cursor.close()
-        db_connect.close()
-
-    except MySQLdb.Error as e:
-        print("MySQL Error:", e)
+    db_cursor.close()
+    db_connect.close()
